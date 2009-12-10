ab153a6d24c003c55dcaff7a3eb03abaea7b0d39
Dr Nic Williams <drnicwilliams@gmail.com>
2009-12-10 13:58:54 +1000

    support for paperclip attachments stored on filesystem if no config/s3.yml available

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -62,7 +62,7 @@ class NewsItem < ActiveRecord::Base
 
   has_attached_file :featured_image,
                     :styles => { :thumb => '50x50#', :medium => "200x150#" },
-                    :path => ":rails_root/public/system/news_items/" <<
+                    :path => "news_items/" <<
                              ":attachment/:id_partition/" <<
                              ":basename_:style.:extension",
                     :url =>  "/system/news_items/:attachment/:id_partition/" <<
