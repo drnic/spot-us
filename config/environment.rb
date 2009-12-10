d48a2d5d00f29e8b8bb490d78266157bafaebefc
Dr Nic Williams <drnicwilliams@gmail.com>
2009-12-10 14:11:00 +1000

    upgraded to latest cucumber

--- config/environment.rb
+++ config/environment.rb
@@ -20,8 +20,6 @@ Rails::Initializer.run do |config|
   config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
   config.gem "rubyist-aasm", :lib => "aasm", :version => '>=2.0.5', :source => 'http://gems.github.com'
   config.gem 'mislav-will_paginate', :lib => 'will_paginate', :version => '>=2.3.7', :source => 'http://gems.github.com/'
-  config.gem "rspec-rails", :lib => false, :version => "= 1.2.2"
-  config.gem "webrat", :lib => false, :version => "= 0.4.4"
   config.gem "money", :version => ">=2.1.3"
 
   config.time_zone = 'UTC'
